e974ae7aa6c163e6a94c3705f3352b359a487b10
Brendon Feitosa <brendonfeitosa@hotmail.com>
2022-05-10 19:54:37 -0300

    Update Ex07Estrutura.py

---
+++ Ex07Estrutura.py
@@ -0,0 +1,6 @@
+'''7. Elabore uma estrutura para representar um Produto (código, nome, data_fabricacao, data_validade, preço). Para o membro data_fabricacao e data_validade, deve-se criar outra estrutura Data (dia, mês, ano). Utilize aninhamento de estruturas para resolver este desenvolvimento. Construa uma função para cada opçao do menu a seguir:
+Menu de opções:
+1. Cadastrar produtos
+2. Visualizar todos os dados
+3. Sair'''
+
